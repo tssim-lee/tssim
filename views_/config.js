db0de0a8edb40ad7d88b76f86178e14b2f0a92ed
tssim-lee <40420058+tssim-lee@users.noreply.github.com>
2018-07-11 15:19:36 +0500

    Update config.js

--- views_/config.js
+++ views_/config.js
@@ -1,7 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 43210,
     BD: process.env.DATABASE_URL,
-
-    //  BD: process.env.DATABASE_URL || 'postgres://tssim:LoraSim_1BD@ex2.cyaqfdljfc4j.us-east-2.rds.amazonaws.com:5432/postgres',
-    // "postgres://username:password@host:port/db_name";
 };
